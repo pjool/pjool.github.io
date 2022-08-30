3796460fe2c5d3bf7d245a6afcc8a2d7ccaa94d3
Peter Olsen <pjol@protonmail.com>
2022-08-30 11:36:23 +0200

    improved clock

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const minuteTarget = 17 // den skal være 17 minutter over
+const minuteTarget = 27; // den skal være 17 minutter over
 
 function updateTime() {
-    var now = new Date()
-    var hours = now.getHours()
-    var minutes = now.getMinutes()
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
 
-    diff = (minuteTarget - minutes + 60 ) % 60
+    diff = (minuteTarget - minutes + 60 ) % 60;
 
-    var timeString = hours.toString() + "." + minutes.toString()
-    document.getElementById("clock").innerHTML = timeString
-    document.getElementById("diff").innerHTML = diff
+    var message;
+    if (diff == 0) {
+        message = "Nu er der rave med de hårde drenge!";
+    }
+    else {
+        let pluralSuffix = (diff == 1) ? "" : "ter";
+        message = `Der er ${diff} minut${pluralSuffix} til rave!`;
+    }
+
+    function toClockSegment(t) {
+        return t.toString().padStart(2, "0");
+    }
+
+    var timeString = toClockSegment(hours) + "." + toClockSegment(minutes) + ":" + toClockSegment(seconds);
+    document.getElementById("clock").innerHTML = timeString;
+    document.getElementById("diff").innerHTML = message;
     }
 
 
